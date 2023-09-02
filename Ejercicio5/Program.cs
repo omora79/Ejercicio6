2e658199e966e935e5681f73d923e8c2329378dc
OSCAR ALBERTO MORA M�NDEZ <oscarmora18268@correo.itm.edu.co>
2023-09-01 19:20:26 -0500

    programa terminado

---
+++ Ejercicio5/Program.cs
@@ -0,0 +1,29 @@
+﻿// Escribir un programa que convierta un valor dado en grados Fahrenheit a grados Celsius.
+
+// 1 clase y metodo
+
+public class Ejercicio6
+{
+    static void Main(string[] args)
+    {
+
+        // 2 definicion de variables
+
+        int farenhait, celcius;
+
+        // pedir datos por pantalla
+
+        Console.WriteLine("Ingrese las grados medidos en Farenhait: ");
+        farenhait = Convert.ToInt32(Console.ReadLine());
+
+        // 4 realizar los calculos
+
+        celcius = ((farenhait - 32) * 5 / 9);
+
+        // 5 mostrar resultados en pantalla
+
+        Console.WriteLine($"La conversion de {farenhait} grados farenhait es de {celcius} grados celcuis");
+
+
+    }
+}
